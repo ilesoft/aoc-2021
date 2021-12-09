f49200d3590a5b50ccc29448761d4d6b588966b2
ile <ilmari.marttila@gmail.com>
2021-12-09 13:52:53 +0200

    There are only 4 of those values => no need for memory eficient int type

--- 04/04-a.cpp
+++ 04/04-a.cpp
@@ -6,18 +6,18 @@ int main()
     std::string s;
     // Let's assume there are maximum of 256 vents per point => uint8_t
     uint8_t seaFloor[1000][1000] = {0};
-    uint16_t position;
-    int count = 0;
+    int position;
+    int pointsWithMoreThanOneVent = 0;
 
     while( getline(std::cin, s) )
     {
         std::string::size_type pos1 = s.find(",");
         std::string::size_type pos2 = s.find(" -> ");
         std::string::size_type pos3 = s.find_last_of(",");
-        uint16_t startX = std::stoul(s);
-        uint16_t startY = std::stoul(s.substr(pos1+1));
-        uint16_t endX   = std::stoul(s.substr(pos2+4));
-        uint16_t endY   = std::stoul(s.substr(pos3+1));
+        int startX = std::stoul(s);
+        int startY = std::stoul(s.substr(pos1+1));
+        int endX   = std::stoul(s.substr(pos2+4));
+        int endY   = std::stoul(s.substr(pos3+1));
 
         if (startX != endX && startY != endY) continue;
 
@@ -31,7 +31,7 @@ int main()
             position = startY;
             while (true)
             {
-                if (++seaFloor[startX][position] == 2) ++count;
+                if (++seaFloor[startX][position] == 2) ++pointsWithMoreThanOneVent;
 
                 if (position == endY) break;
 
@@ -46,7 +46,7 @@ int main()
             position = startX;
             while (true)
             {
-                if (++seaFloor[position][startY] == 2) ++count;
+                if (++seaFloor[position][startY] == 2) ++pointsWithMoreThanOneVent;
 
                 if (position == endX) break;
 
@@ -56,7 +56,7 @@ int main()
         }
     }
 
-    std::cout << count << std::endl;
+    std::cout << pointsWithMoreThanOneVent << std::endl;
 
     return EXIT_SUCCESS;
 }
